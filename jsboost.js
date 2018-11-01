1970e817ba19d6e4e3a3251f0a120886e55c6775
brchen777 <eric790620@gmail.com>
2018-11-01 17:10:22 +0800

    Add exports KJUR

--- jsboost.js
+++ jsboost.js
@@ -8,7 +8,7 @@
 	const {EventEmitter}	= require( './native/event-emitter' );
 	const {DOMEventEmitter} = require( './native/dom-event-emitter' );
 	const {AsyncInvoke}		= require( './native/async-invoke' );
-	const {Signature, ECDSA, KEYUTIL} = require( './native/crypto' );
+	const {KJUR, Signature, ECDSA, KEYUTIL} = require( './native/crypto' );
 	const {UInt64, Int64}	= require( './native/uint64' );
 	const {UInt128, Int128}	= require( './native/uint128' );
 	const Base64URL = require( './node/base64url' );
@@ -21,6 +21,7 @@
 		EventEmitter,
 		DOMEventEmitter,
 		AsyncInvoke,
+		KJUR,
 		Signature,
 		ECDSA,
 		KEYUTIL,
