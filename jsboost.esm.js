0abe3de8b33b03d56315b330c11d628858f6d787
JCloudYu <j.cloud.yu@gmail.com>
2018-12-29 16:52:16 +0800

    Add ext-object library

--- jsboost.esm.js
+++ jsboost.esm.js
@@ -16,6 +16,7 @@ export * from "./lib/throttled-queue.esm.js";
 export * from "./lib/ext-type.esm.js";
 export * from "./lib/event-emitter.esm.js";
 export * from "./lib/dom-event-emitter.esm.js";
+export * from "./lib/ext-object.esm.js";
 
 
 
