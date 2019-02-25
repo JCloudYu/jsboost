6e6175fb487e629aff4bb9cabc4fa6c0e69173a3
JCloudYu <j.cloud.yu@gmail.com>
2019-02-26 01:11:17 +0800

    Prevent errors caused by loading browser-only modules

--- jsboost.esm.js
+++ jsboost.esm.js
@@ -2,23 +2,7 @@
  *	Author: JCloudYu
  *	Create: 2018/12/22
 **/
-import * as ANSIColor from "./lib/ansi-vt100-colors.esm.js";
+export * from "./jsboost-core.esm.js";
 import * as Web from "./lib/web/web.esm.js";
 
-export * from "./lib/base32.esm.js";
-export * from "./lib/base64.esm.js";
-export * from "./lib/ext-promise.esm.js";
-export * from "./lib/ext-timer.esm.js";
-export * from "./lib/js-assert.esm.js";
-export * from "./lib/levelfy-logger.esm.js";
-export * from "./lib/state-machine.esm.js";
-export * from "./lib/throttled-queue.esm.js";
-export * from "./lib/event-emitter.esm.js";
-export * from "./lib/dom-event-emitter.esm.js";
-export * from "./lib/ext-object.esm.js";
-export * from "./lib/string-cache.esm.js";
-
-
-
-export { ANSIColor, Web };
-
+export { Web };
--- jsboost.mjs
+++ jsboost.mjs
@@ -2,5 +2,5 @@
  *	Author: JCloudYu
  *	Create: 2018/12/22
 **/
-export * from "./jsboost.esm.js";
+export * from "./jsboost-core.esm.js";
 export * from "./lib/node/stream-reader.esm.js";
