c04dc6b62d0b6970838d092ad0996eaa40b87a64
JCloudYu <j.cloud.yu@gmail.com>
2019-05-04 21:36:13 +0800

    Update esm.js loader to latest version

--- ._/loader.mjs
+++ ._/loader.mjs
@@ -12,18 +12,45 @@ import os from 'os';
 
 
 
-const IS_WINDOWS = (os.platform().substring(0,3).toLowerCase() === "win");
+const IS_WINDOWS = (os.platform().substring(0,3) === "win");
 const IS_WIN_ABSOLUTE_PATH = /^[a-zA-Z]:\/[^/].*$/;
-const BASE_URL = `file://${IS_WINDOWS?'/':''}${process.cwd()}/`;
+const PATHS = [
+	null,	// Reserved for main module dir
+	null,	// Reserved for main module path
+	`file://${IS_WINDOWS?'/':''}${process.cwd()}/`
+];
 
 export function resolve(specifier, parentModuleURL, defaultResolve) {
+	// This specifier must be the main module with absolute path! (Without leading file://)
+	if ( parentModuleURL === undefined ) {
+		/**
+		 * We don't need to detect the leading C:/ in windows env here...
+		 * It has been processed to /C:/Users/XXX/Desktop/xxx.mjs already...awkward = =+
+		**/
+		const _MAIN_MODULE_PATH = PATHS[1] = `file://${specifier}`;
+		const DIVIDER_POS = _MAIN_MODULE_PATH.lastIndexOf('/')+1;
+		PATHS[0] = _MAIN_MODULE_PATH.substring(0, DIVIDER_POS);
+		specifier = `./${_MAIN_MODULE_PATH.substring(DIVIDER_POS)}`;
+	}
+	
+	
+	
+	// Prevent falsy absolute path in Windows environment ( such as C:/ )
 	if ( IS_WINDOWS && IS_WIN_ABSOLUTE_PATH.test(specifier) ) {
 		specifier = `file:///${specifier}`;
 	}
 
+	// Path started with / means the path init from the directory of main module
+	// This mechanism is used to follow the rule in browser environment ( path started from / means from host root )
+	if ( specifier[0] === "/" ) {
+		specifier = `${PATHS[0]}${specifier.substring(1)}`;
+	}
+	
+	
+	
 	if ( specifier.substr(-7) === ".esm.js" ) {
 		return {
-			url: new URL(specifier, parentModuleURL||BASE_URL).href,
+			url: new URL(specifier, parentModuleURL||PATHS[0]).href,
 			format: 'esm'
 		};
 	}
